c08c29afb99dcddf1ffcb223f0e99d831e0843a7
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2025-03-04 15:26:53 +0300

    for loops

--- Control.py
+++ Control.py
@@ -4,3 +4,23 @@ if temperature > 25:
     print("It’s a hot day! ☀️")
 else:
     print("It’s a cool day! ❄️")
+
+
+
+  # 💡 You can also add elif for multiple conditions:
+
+# Control flow example for grading student marks
+# This will ask the user for input and grade the student's performance
+
+# Prompt the user for student marks
+marks = int(input("Enter the student's marks (0-100): "))
+
+# Grading system based on the marks
+if marks > 70:
+    print("Grade: Distinction 🏆")
+elif marks >= 60:
+    print("Grade: Credit 🎖️")
+elif marks >= 50:
+    print("Grade: Pass 👍")
+else:
+    print("Grade: Fail ❌")
---
+++ loops.py
@@ -0,0 +1,12 @@
+#For loop:  Used for repeating tasks over a range. without setting any/much conditions---Repeats a block of code a fixed number of times.
+
+# Print numbers 0 to 4
+for i in range(5):
+    print(i)
+
+# Outputs:
+# 0
+# 1
+# 2
+# 3
+# 4
