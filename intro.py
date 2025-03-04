449cef3b0616f0b3a72818fff4ccebd7f30cb42f
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2025-03-04 15:04:25 +0300

    control flows in python

--- intro.py
+++ intro.py
@@ -1,7 +1,7 @@
 # Variables hold data
 age = 25          # int (integer)
 height = 5.75      # float (decimal)
-name = "Charlie"   # str (string)
+name = "Charlie"   # str (string) special characters are also accounted for as string
 is_student = True  # bool (boolean)
 
 # Printing out the values
@@ -9,3 +9,11 @@ print("Name:", name)          # Outputs: Name: Charlie
 print("Age:", age)            # Outputs: Age: 25
 print("Height:", height)      # Outputs: Height: 5.75
 print("Is a student?", is_student)  # Outputs: Is a student? True
+
+
+# Check Data Type
+ # (Type is a method that reveals what kind of data you're using')
+print(type(age))  #Output: <class 'int'>
+print(type(height))
+print(type(name))
+print(type(is_student))
