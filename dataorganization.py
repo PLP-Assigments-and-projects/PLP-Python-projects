6e612a7c3450ddc25a4208fc4498a7251b422f26
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2025-03-04 15:43:49 +0300

    tuples in python

--- dataorganization.py
+++ dataorganization.py
@@ -17,3 +17,17 @@ print(fruits)  # Outputs: ['apple', 'orange', 'cherry']
 # Adding an element
 fruits.append("grape")
 print(fruits)  # Outputs: ['apple', 'orange', 'cherry', 'grape']
+
+
+# Tuples 🔒
+# Similar to lists but immutable (unchangeable).
+
+# Tuples are like lists, but unchangeable. Once created, you can’t modify them.
+
+coordinates = (10, 20)
+
+# Accessing elements
+print(coordinates[0])  # Outputs: 10
+
+# Trying to modify a tuple will raise an error:
+# coordinates[0] = 5  # Error! Tuples can't be modified.
